bf87124a1935faa7b9504d2762a264f9305aff06
zhangjie <137548913@qq.com>
2020-05-07 23:01:09 +0800

    file operator

--- main/main.go
+++ main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 const textPath string = "/Users/zhangjie/Desktop/selfCode/gobasic/files"
@@ -51,9 +52,38 @@ func osRemove() {
 }
 
 func ReaderTest() {
+	r := strings.NewReader("hello 世界")
+	b := make([]byte, r.Size())
+	n, err := r.Read(b)
+	if err != nil {
+		fmt.Println("error", err)
+		return
+	}
+	fmt.Println("读取的长度为", n)
+	fmt.Println("数据内容为", string(b))
+}
 
+func ReaderFile() {
+	f, _ := os.Open(textPath + "/b/a.txt")
+	fileinfo, _ := f.Stat()
+	b := make([]byte, fileinfo.Size())
+	n, _ := f.Read(b)
+	fmt.Println("读取的长度为", n)
+	fmt.Println("数据内容为", string(b))
+}
+
+func writeFile() {
+	filepath := textPath + "/b/b.txt"
+	f, err := os.OpenFile(filepath, os.O_APPEND, 0666)
+	if err != nil {
+		f, err = os.Create(filepath)
+	}
+	f.Write([]byte("展望"))
+	defer func() {
+		f.Close()
+	}()
 }
 
 func main() {
-	ReaderTest()
+	ReaderFile()
 }
